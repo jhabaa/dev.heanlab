68834de0c29bfe01f16be8967be80c4ec1f5598e
Hean <jhubert@heanlab.com>
2025-07-05 23:28:06 +0200

    [FIX]: URL should be meta URL

--- src/views/ProjectDetailsView.vue
+++ src/views/ProjectDetailsView.vue
@@ -32,9 +32,7 @@ async function onLoad() {
 
 function getImageurl(_image: string | undefined) {
   if (!_image) return;
-  console.log(_image);
-  const img_url = `/src/assets/images/${_image}`;
-  return img_url;
+  return new URL(`/src/assets/images/${_image}`, import.meta.url).href;
 }
 
 onMounted(async () => {
